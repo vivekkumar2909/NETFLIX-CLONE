8d7d672552b7e84d1d0e099e7faa02b7994f39fe
vivekkumar2909 <rajvivek620065@gmail.com>
2024-02-18 21:33:20 +0530

    "Auth"

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import  { useState } from 'react'
 import Image from 'next/image'
 import Inputs from '@/components/Inputs'
 
 export default function auth() {
+
+  const [email,setEmail] = useState('');
+  const [username,setUsername] = useState('');
+  const [password,setPassword] = useState('');
+  console.log(email);
+
   return (
     <div className='relative h-full w-full bg-[url(/images/hero.jpg)] bg-center bg-fixed bg-cover'>
       <div className='bg-black w-full h-full lg:bg-opacity-50'>
@@ -13,13 +19,36 @@ export default function auth() {
         </nav>
 
         <div className='flex justify-center'>
-          <div className='bg-black bg-opacity-70 p-16 self-center mt-3 lg:w-2/5 lg:max-w-md rounded-xl w-full'>
+          <div className='bg-black bg-opacity-70 p-16 self-center mt-3 lg:w-2/5 lg:max-w-md rounded-2xl w-full'>
             <h2 className='text-white text-4xl mb-8 font-semibold text-center'>
                Sign In
             </h2>
-            <div className='flex flex-col gap-4'>
-              <Inputs/>
+            <div className='flex flex-col gap-4 '>
+              <Inputs 
+              label="Username"
+              onChange={(e : any)=>{setUsername(e.target.value)}}
+              id="username"
+              type='name'
+              value ={username}
+               />
+              <Inputs 
+              label="Email"
+              onChange={(e :any)=>{setEmail(e.target.value)}}
+              id="email"
+              type='email'
+              value ={email}
+               />
+              <Inputs 
+              label="Password"
+              onChange={(e : any)=>{setPassword(e.target.value)}}
+              id="password"
+              type='password'
+              value ={password}
+               />
             </div>
+            <button className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'>
+              Login
+            </button>
           </div>
         </div>
       </div>
